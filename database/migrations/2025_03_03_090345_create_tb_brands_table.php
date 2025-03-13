58b908c0355484193868aba89a2fef2b03383f85
NamaKamu <emailkamu@example.com>
2025-03-13 22:38:34 +0700

    fix:all migration can nullable

--- database/migrations/2025_03_03_090345_create_tb_brands_table.php
+++ database/migrations/2025_03_03_090345_create_tb_brands_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tb_brands', function (Blueprint $table) {
             $table->id();
             $table->string('brand_name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2025_03_03_090824_create_tb_units_table.php
+++ database/migrations/2025_03_03_090824_create_tb_units_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tb_units', function (Blueprint $table) {
             $table->id();
             $table->string('unit_name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
